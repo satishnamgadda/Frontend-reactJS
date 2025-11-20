cdd797e11621b43795b2cd153fbb844aa3b4bd0c
maramvenkatareddy <113689196+maramvenkatareddy@users.noreply.github.com>
2025-11-20 10:06:17 +0530

    Create api.js

---
+++ src/services/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const userService = {
+  getAll: () => api.get('/users'),
+  getById: (id) => api.get(`/users/${id}`),
+  create: (userData) => api.post('/users', userData),
+  update: (id, userData) => api.put(`/users/${id}`, userData),
+  delete: (id) => api.delete(`/users/${id}`),
+};
+
+export default api;
